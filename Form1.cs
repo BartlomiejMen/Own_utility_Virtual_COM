9a51d8414caa16418f5368edc118f004871d1808
Bartlomiej Men <bartlomiej.men@hotmail.com>
2022-03-24 21:21:26 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace COM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboPort.Items.AddRange(ports);
+            try
+            {
+                cboPort.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Brak dostępnego portu. "); // + ex.Message
+
+            }
+           
+            btnClose.Enabled = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            try
+            {
+                serialPort1.PortName = cboPort.Text;
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine(txtMessage.Text + Environment.NewLine);
+                    txtMessage.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    txtReceive.Text = serialPort1.ReadExisting();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+    }
+}
